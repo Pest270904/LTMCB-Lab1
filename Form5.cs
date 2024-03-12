f525413aec0cbca1ecc505c06193250c28886170
Pest270904 <ntpllc2004@gmail.com>
2024-03-12 23:03:20 +0700

    fix bugs on Form5

--- Form5.cs
+++ Form5.cs
@@ -15,19 +15,18 @@ namespace bt
     public partial class Form5 : Form
     {
         public static string placeHolder = "Enter a list of scores, can only be seperated by a space or comma     Ex: 5.75 3 5";
+        public static string errorMsg = "- Please re-enter your list of scores that only contain non-negative numbers " +
+                    "which is <= 10 and not having more than 2 floating digits" +
+                    "\n- Each one is separated by a space or comma.";
         private static bool isLegitInput(string text)
         {
-            bool check1 = text.All("0123456789,. ".Contains);
+            //bool check1 = text.All("0123456789,. ".Contains);
             string[] seperate = { " ", ", ", "," };
             string[] arr = text.Split(seperate, System.StringSplitOptions.RemoveEmptyEntries);
-            bool check2 = true;
             foreach (var each in arr)
-                if (Convert.ToDouble(each) > 10 || each.Length>4)
-                    {
-                        check2 = false;
-                        break; 
-                    }
-            return check1 && check2;
+                if (Convert.ToDouble(each) > 10 || (Convert.ToDouble(each) == 10 ? each.Length > 5 : each.Length > 4))
+                    return false;
+            return true;
         }
         public Form5()
         {
@@ -64,49 +63,53 @@ namespace bt
             failTB.Clear();
             finalTB.Clear();
 
-            if (inputTB.Text != placeHolder && isLegitInput(inputTB.Text) && String.IsNullOrEmpty(inputTB.Text)==false)
-            {
-                string[] seperate = { " ", ", ", "," };
-                string[] listOfScores = inputTB.Text.Split(seperate, System.StringSplitOptions.RemoveEmptyEntries);
-                int i = 1, pass = 0, fail = 0;
-                double avg = 0, dMin = 11, dMax = 0;
-                foreach (var score in listOfScores)
+            try {
+                if (isLegitInput(inputTB.Text))
                 {
-                    double dScore = Convert.ToDouble(score);
-                    if (dScore < 5)
-                        fail++;
-                    else pass++;
-                    avg += dScore;
-                    dMin = Math.Min(dMin, dScore);
-                    dMax = Math.Max(dMax, dScore);
+                    string[] seperate = { " ", ", ", "," };
+                    string[] listOfScores = inputTB.Text.Split(seperate, System.StringSplitOptions.RemoveEmptyEntries);
+                    int i = 1, pass = 0, fail = 0;
+                    double avg = 0, dMin = 11, dMax = 0;
+                    foreach (var score in listOfScores)
+                    {
+                        double dScore = Convert.ToDouble(score);
+                        if (dScore < 5)
+                            fail++;
+                        else pass++;
+                        avg += dScore;
+                        dMin = Math.Min(dMin, dScore);
+                        dMax = Math.Max(dMax, dScore);
 
-                    listScoreTB.Text += ("Môn học " + i.ToString() + ": " + dScore.ToString() + "đ" + '\t');
-                    if (i % 5 == 0)
-                        listScoreTB.AppendText(Environment.NewLine);
-                    i++;
+                        listScoreTB.Text += ("Môn học " + i.ToString() + ": " + dScore.ToString() + "đ" + '\t');
+                        if (i % 5 == 0)
+                            listScoreTB.AppendText(Environment.NewLine);
+                        i++;
+                    }
+
+                    avgTB.Text = "Điểm trung bình: " + (avg / (i - 1)).ToString("0.00");
+                    maxScoreTB.Text = "Điểm cao nhất: " + dMax.ToString();
+                    minScoreTB.Text = "Điểm thấp nhất: " + dMin.ToString();
+                    passTB.Text = "Số môn đậu: " + pass.ToString();
+                    failTB.Text = "Số môn rớt: " + fail.ToString();
+
+                    if (avg >= 8 && dMin >= 6.5)
+                        finalTB.Text = "Xếp loại học lực: Giỏi";
+                    else if (avg >= 6.5 && dMin >= 5)
+                        finalTB.Text = "Xếp loại học lực: Khá";
+                    else if (avg >= 5 && dMin >= 3.5)
+                        finalTB.Text = "Xếp loại học lực: TB";
+                    else if (avg >= 3.5 && dMin >= 2)
+                        finalTB.Text = "Xếp loại học lực: Yếu";
+                    else
+                        finalTB.Text = "Xếp loại học lực: Kém";
                 }
-
-                avgTB.Text = "Điểm trung bình: " + (avg / (i - 1)).ToString("0.00");
-                maxScoreTB.Text = "Điểm cao nhất: " + dMax.ToString();
-                minScoreTB.Text = "Điểm thấp nhất: " + dMin.ToString();
-                passTB.Text = "Số môn đậu: " + pass.ToString();
-                failTB.Text = "Số môn rớt: " + fail.ToString();
-
-                if (avg >= 8 && dMin >= 6.5)
-                    finalTB.Text = "Xếp loại học lực: Giỏi";
-                else if (avg >= 6.5 && dMin >= 5)
-                    finalTB.Text = "Xếp loại học lực: Khá";
-                else if (avg >= 5 && dMin >= 3.5)
-                    finalTB.Text = "Xếp loại học lực: TB";
-                else if (avg >= 3.5 && dMin >= 2)
-                    finalTB.Text = "Xếp loại học lực: Yếu";
-                else 
-                    finalTB.Text = "Xếp loại học lực: Kém";
+                else
+                    MessageBox.Show(errorMsg);
             }
-            else
-                MessageBox.Show("- Please re-enter your list of scores that only contain non-negative numbers " +
-                    "which is <= 10 and not having more than 2 floating digits" +
-                    "\n- Each one is separated by a space or comma.");
+            catch(Exception ex)
+            {
+                MessageBox.Show(errorMsg);
+            }
         }
     }
 }
